743b117aa2e40bd1a21eb2eec9d79bea3ca080f1
josue <josue.mlpro.2014@gmail.com>
2025-06-25 23:24:13 -0500

    agregando funcionalidad: agregar contacto

--- gestion_de_contactos.cpp
+++ gestion_de_contactos.cpp
@@ -15,7 +15,11 @@ struct contactoEmail{
 
 int main(){
 	
-	int hacer;
+	const int maximo=50;
+	int hacer, i=0 ;
+	
+	contactoEmail persona[50];
+	
 	//mostramos el menu de entrada
 	cout<<"menu: "<<endl;
 	cout<<endl;
@@ -26,9 +30,30 @@ int main(){
 	cout<<"que deseas hacer: "<<endl;
 	cin >> hacer;
 	
+	cin.ignore();
 	//comparamos la respuesta
 	if (hacer==1){
 		
+		//agregando datos de los contactos
+		cout<<"ingresa el nombre de la persona "<<endl;
+		getline(cin, persona[i].nombres);
+		cout<<endl;
+		cout<<"ingrese el sexo de las persona(M-F)"<<"\n";
+		cin >> persona[i].sexo ;
+		cout<<endl;
+		cout<<"ingrese la edad de la persona"<<endl;
+		cin>> persona[i].edad;
+		cout<<endl;
+		cout<<"ingrese el numero de las persona"<<endl;
+		cin>> persona[i].telefono;
+		cout<<endl;
+		cout<<"ingrese el email de la persona"<<endl;
+		cin>> persona[i].email;
+		cout<<endl;
+		cout<<"ingrese la nacionalidad de la personas"<<endl;
+		cin>> persona[i].nacionalidad;
+		
+		i++;
 	}
 	return 0;
 }
