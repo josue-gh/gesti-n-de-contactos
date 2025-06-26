c532fb291c213ba264e29ecb8e1a05f93fc71625
josue <josue.mlpro.2014@gmail.com>
2025-06-25 19:04:47 -0500

    avance 2-agregando estructura y pidiendo dato

--- gestion_de_contactos.cpp
+++ gestion_de_contactos.cpp
@@ -2,3 +2,33 @@
 #include<string>
 
 using namespace std; 
+
+struct contactoEmail{
+	
+	string nombres;
+	char sexo;
+	int edad;
+	string telefono;
+	string email;
+	string nacionalidad;
+};
+
+int main(){
+	
+	int hacer;
+	//mostramos el menu de entrada
+	cout<<"menu: "<<endl;
+	cout<<endl;
+	cout<<"agregar nuevo contacto = 1"<<endl;
+	
+	
+	//pedimos lo que desea hacer
+	cout<<"que deseas hacer: "<<endl;
+	cin >> hacer;
+	
+	//comparamos la respuesta
+	if (hacer==1){
+		
+	}
+	return 0;
+}
